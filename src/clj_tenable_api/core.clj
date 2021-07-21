95b214d9cc1576f128a20d178064fd245f15bc80
Benjamin Singleton <bsinglet@vt.edu>
2021-07-21 10:11:04 -0400

    Improved the SC example to return a Clojure map of the json response. Also added function to cut these maps down only to the desired keys.

--- src/clj_tenable_api/core.clj
+++ src/clj_tenable_api/core.clj
@@ -12,11 +12,22 @@
 
 (defn tenable-sc-ignore-certs
   "Example call to a local Tenable.SC server, ignoring the fact that
-  it's using self-signed certs."
+  it's using self-signed certs. This example lists out the users on
+  the server."
   [access-key secret-key]
-  (client/get "https://192.168.50.201/rest/user?fields=apiKeys%2Cname%2Cusername%2Cfirstname%2Clastname%2Cgroup%2Crole%2ClastLogin%2CcanManage%2CcanUse%2Clocked%2Cstatus%2Ctitle%2Cemail%2Cid%2CauthType" {:insecure? true
+  (get-in
+    (client/get "https://192.168.50.201/rest/user?fields=apiKeys%2Cname%2Cusername%2Cfirstname%2Clastname%2Cgroup%2Crole%2ClastLogin%2CcanManage%2CcanUse%2Clocked%2Cstatus%2Ctitle%2Cemail%2Cid%2CauthType" {:insecure? true
+    :as :json
     :headers {"Accept" "application/json",
-    "x-apikey" (str "accessKey=" access-key ";secretKey=" secret-key)}}))
+    "x-apikey" (str "accessKey=" access-key ";secretKey=" secret-key)}})
+    [:body :response]))
+
+  (defn map-usernames-to-ids
+    "Takes a vector of hash-maps. Each map has the data for a Tenable.SC
+    user. These maps have a lot of extraneous keys, so this function drops
+    everything except the username and id."
+    [users]
+    (map #(hash-map :username (:username %), :id (:id %)) users))
 
 ;; (client/put "http://example.com/api" {:body "my PUT body"})
 
@@ -32,5 +43,6 @@
 (defn -main
   "Make a simple http request."
   [& args]
-  (println (get-users-list "" ""))
-  (println (tenable-sc-ignore-certs "" "")))
+  println
+    (map-usernames-to-ids
+      (tenable-sc-ignore-certs "" "")))
