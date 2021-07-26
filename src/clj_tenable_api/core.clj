e3512191f362d9aa2bdf801553de543d89852801
Benjamin Singleton <bsinglet@vt.edu>
2021-07-26 13:49:33 -0400

    Added functionality for running Vuln Analysis queries in Tenable.SC. Also, did some refactoring to make utility functions more useful.

--- src/clj_tenable_api/core.clj
+++ src/clj_tenable_api/core.clj
@@ -43,7 +43,8 @@
     [:body :response :scanResult :id]))
 
 (defn generate-active-scan-body
-  ""
+  "Generates the payload for a Tenable.SC Active Scan based on a given
+  policy ID and a list of target IPs."
   ([scan-name policy-id ip-list]
     {:name scan-name
       :ipList ip-list
@@ -55,12 +56,17 @@
     (generate-active-scan-body "Test scan" "1000003" "192.168.8.161")))
 
 (defn stringify-active-scan-body
-  ""
+  "Casts a Clojure map (possibly containing maps and vectors) as a
+  string, converting the key values to json-like keys and inserting
+  commas between consecutive maps."
   [body]
-  (clojure.string/replace (str body) #"\:(\S+)" "\"$1\":"))
+  (clojure.string/replace
+    (clojure.string/replace (str body) #"\:(\S+)" "\"$1\":")
+    #"\}\s\{" "}, {"))
 
 (defn tenable-sc-create-scan
-  ""
+  "Creates a new Active Scan in Tenable.SC, using the
+  generate-active-scan-body function."
   [access-key secret-key]
   (get-in
     (client/post
@@ -73,6 +79,47 @@
             })
     [:body :response :id]))
 
+
+(defn generate-analysis-query
+  "Generates a Tenable.SC Vulnerability Analysis query, filtering on
+  hostname and plugin ID, specifically using the
+  'Vulnerability Detail List' view."
+  [hostname plugin_id]
+  {
+    :type "vuln"
+    :sourceType "cumulative"
+    :query {
+      :startOffset "0"
+      :endOffset "50"
+      :type "vuln"
+      :vulnTool "vulndetails"
+      :tool "vulndetails"
+  :filters [
+    {
+      :id "ip", :filterName "ip", :operator "=",
+      :type "vuln" :isPredefined "true",
+      :value hostname
+    },
+    {
+      :id "pluginID", :filterName "pluginID", :operator "=",
+      :type "vuln", :isPredefined "true", :value plugin_id}]}})
+
+(defn tenable-sc-vuln-analysis
+  "Runs a vulnerability analysis query based on hostname and plugin ID.
+  This query is generated through the generate-analysis-query function,
+  which specifically uses the 'Vulnerability Detail List' view."
+  [access-key secret-key hostname plugin_id]
+  (get-in
+    (client/post
+      "https://192.168.50.201/rest/analysis"
+        {:insecure? true
+          :as :json
+          :headers {"Accept" "application/json", "x-apikey"
+            (str "accessKey=" access-key ";secretKey=" secret-key)}
+          :body (stringify-active-scan-body (generate-analysis-query hostname plugin_id))
+            })
+    [:body]))
+
 ;; (client/put "http://example.com/api" {:body "my PUT body"})
 
 (defn -main
@@ -83,8 +130,10 @@
     (println
       (map-usernames-to-ids
         (tenable-sc-list-users (nth keys 0) (nth keys 1))))
-    (println (str "Launching scan with scan result ID "
-      (tenable-sc-launch-scan (nth keys 0) (nth keys 1) 1)))
-    (println (str "Creating scan with Active Scan ID "
-      (tenable-sc-create-scan (nth keys 0) (nth keys 1))))
+    ;;(println (str "Launching scan with scan result ID "
+    ;;  (tenable-sc-launch-scan (nth keys 0) (nth keys 1) 1)))
+    ;;(println (str "Creating scan with Active Scan ID "
+    ;;  (tenable-sc-create-scan (nth keys 0) (nth keys 1))))
+    (println (str "Querying plugin 19506 on host 192.168.8.161 "
+      (tenable-sc-vuln-analysis (nth keys 0) (nth keys 1) "192.168.8.161" "19506")))
     ))
